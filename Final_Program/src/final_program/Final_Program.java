820073890013a463bb551a877f4a9b7c17a1acd0
amador-e-rivera <amador.e.rivera@gmail.com>
2015-04-25 13:03:53 -0700

    Copied and pasted the code in the 3D Viewing slides and it created a functioning 3D viewing space.  The only code I typed in was:
    1. The cube coordinates.
    2. Line 50 in the Final_Program.java file. Without the glEnable(GL_DEPTH_TEST), the cube was being generated with no depth perception.
    3. Very Minor fixes here and there due to the copy and pasting.

---
+++ Final_Program/src/final_program/Final_Program.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package final_program;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.util.glu.GLU;
+
+/**
+ *
+ *
+ */
+public class Final_Program {
+
+    private FPCameraController fp = new FPCameraController(0f, 0f, 0f);
+    private DisplayMode displayMode;
+
+    public void start() {
+        try {
+            createWindow();
+            initGL();
+            fp.gameLoop(); //render();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createWindow() throws Exception {
+        Display.setFullscreen(false);
+        DisplayMode d[] = Display.getAvailableDisplayModes();
+        for (int i = 0; i < d.length; i++) {
+            if (d[i].getWidth() == 640
+                    && d[i].getHeight() == 480
+                    && d[i].getBitsPerPixel() == 32) {
+                displayMode = d[i];
+                break;
+            }
+        }
+        Display.setDisplayMode(displayMode);
+        Display.setTitle("Final Program - Checkpoin 1");
+        Display.create();
+    }
+
+    private void initGL() {
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glEnable(GL_DEPTH_TEST);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        GLU.gluPerspective(100.0f, (float) displayMode.getWidth() / 
+                (float) displayMode.getHeight(), 0.1f, 300.0f);
+        glMatrixMode(GL_MODELVIEW);
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Final_Program final_Program = new Final_Program();
+        final_Program.start();
+    }
+}
